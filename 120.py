641099360cec78c2ba6c0428b62415341d34f864
Felipe Zen Thomaz dos Santos <60949983+SemFlahsPf@users.noreply.github.com>
2020-03-03 22:03:27 -0300

    first commit

---
+++ 120.py
@@ -0,0 +1,9 @@
+#AUTORES: 1DSI3 Felipe Thomaz
+#FEITO EM: 03/03/2020
+#instituiçao: ETEC
+X = 0 #declaraçao de contador
+VALOR = 2 #declaraçao de variavel/valor
+WHILE (X < 21):
+       VALOR = VALOR * VALOR
+       X = X + 1  #laço de repetiçao
+       PRINT (VALOR) #exibir variavel valor 
